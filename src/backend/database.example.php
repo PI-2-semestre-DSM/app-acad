e3593f1c38819aea3e419a4a7b3376872676d051
Juliano Santos <julianosantosdev@outlook.com>
2024-11-12 14:55:36 -0300

    add: connection DB class

---
+++ src/backend/database.example.php
@@ -0,0 +1,8 @@
+<?php
+// 01 Preencha o arquivo com os dados do servidor e banco de dados locais;
+// 02 Renomeie o arquivo para 'database.php';
+// 03 Esse arquivo é local somente. Não será commitado nem enviado por git push;
+$database = "";
+$host = "";
+$user = "";
+$password = "";
