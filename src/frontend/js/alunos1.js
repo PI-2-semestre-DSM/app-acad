9f09e38f3808fee9578de31d1e1315e5862fd1c6
RashiniK <162806612+RashiniK@users.noreply.github.com>
2024-12-01 15:14:52 -0300

    Update alunos1.js

--- src/frontend/js/alunos1.js
+++ src/frontend/js/alunos1.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const alunosLink = document.getElementById("link-alunos");
     alunosLink.addEventListener("click", function(event) {
         event.preventDefault(); 
-        window.location.href = "alunos.html"; 
+        window.location.href = "alunos.php"; 
     });
 });
