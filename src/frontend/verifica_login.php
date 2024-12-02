5f402ce3eb0b2330f5716639c0c2e124918161a5
Kauê Rashini <kaue.gomes01@fatec.sp.gov.br>
2024-12-01 21:06:54 -0300

    adicionar produtos e alteraçao em verifica login

--- src/frontend/verifica_login.php
+++ src/frontend/verifica_login.php
@@ -1,18 +1,21 @@
 <?php
-require('../backend/classes/login.class.php');
+require_once('../backend/classes/login.class.php');
 
-if (isset($_POST['login'])) {
+if (isset($_POST['submit'])) {
     $login = $_POST["login"];
     $senha = $_POST["senha"];
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $verifica = new Login();
         if ($verifica->verificarCredenciais($login, $senha)) {
+            $verifica->__destruct();
             header("Location: pagina_inicial.php");
             exit();
         }
+        $verifica->__destruct();
     }
 } else {
     echo "nao existe?";
+    
 }
 
 ?>
